271749358c5c8a9b44ebc1c3ad42c31886eaead4
David Lara <david.a.lara05@gmail.com>
2022-11-04 15:24:16 -0500

    initialize repo

---
+++ tables.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS users;
+
+-- this creates a table in SQL
+  CREATE TABLE users (
+  user_id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(35) NOT NULL,
+  last_name VARCHAR(65)  NOT NULL,
+  PRIMARY KEY (user_id)
+);
+
+
+
+
+
