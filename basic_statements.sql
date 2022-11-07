fb2cfc1fdc251a21794f8daf0661fa94d3ce92bd
David Lara <david.a.lara05@gmail.com>
2022-11-07 09:41:47 -0600

    Created data in album

--- basic_statements.sql
+++ basic_statements.sql
@@ -1,1 +1,3 @@
-INSERT INTO albums (artist, record_name, release_date, sales, genre) VALUES ()
+USE codeup_test_db;
+
+INSERT INTO albums (artist, record_name, release_date, sales, genre) VALUES ('Tupac', '2Pacalyse Now!', 1991, 0.923, 'hiphop');
