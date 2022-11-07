8959ac0b486ea218caeb6bc561e7f76dd77725da
David Lara <david.a.lara05@gmail.com>
2022-11-07 12:12:43 -0600

    finish select exercises

---
+++ albums_seeder.sql
@@ -0,0 +1,51 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, record_name, release_date, sales, genre) VALUES
+('Michael Jackson', 'Thriller', 1982, 50.2, 'Pop, post-disco, funk, rock'),
+('AC/DC', 'Back in Black', 1980, 30.1, 'Hard rock'),
+('Whitney Houston', 'The Bodyguard', 1992, 28.7,'R&B, soul,pop, soundtrack'),
+('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.8, 'Progressive rock'),
+('Eagles', 'Their Greatest Hits (1971 - 1975)', 1976, 41.2, 'Country Rock,Soft Rock, Folk Rock'),
+('Eagles', 'Hotel California', 1976, 31.8, 'Soft Rock'),
+('Shania Twain', 'Come On Over', 1997, 30.4, 'Country,Pop'),
+('Fleetwood Mac', 'Rumours', 1977, 29.3, 'Soft Rock'),
+('Bee Gees', 'Saturday Night Fever', 1977, 22.1, 'Disco'),
+
+('Led Zeppelin', 'Led Zeppelin IV', 1971, 30.4, 'Hard rock, heavy metal, folk rock'),
+('Michael Jackson', 'Bad', 1987, 21.1, 'Pop,rhythm and blue, funk and rock'),
+('Meat Loaf', 'Bat Out of Hell', 1977, 22, 'Hard Rock, glam rock'),
+('Alanis Morissette', 'Jagged Little Pill', 1995, 25.4, 'Alternative rock'),
+('Various artist', 'Dirty Dancing', 1987, 24.1, 'Pop,rock,R&B'),
+('Celine Dion', 'Falling into You', 1996, 21.1, 'Pop,soft rock'),
+('Adele', '21', 2011, 27.1, 'Pop, soul'),
+('The Beatles', '1', 2000, 23.2, 'Rock'),
+('Metallica', 'Metallica', 1991, 22.7, 'Heavy metal'),
+('Celine Dion', 'Lets Talk About Love', 1997, 19.5, 'Pop, soft rock'),
+('Bob Marley & The Wailers', 'Legend: The Best of Bob Marley & The Wailers', 1984, 22.9, 'Reggae'),
+('Guns N Roses', 'Appetite for Destruction', 1987, 22.8, 'Hard rock'),
+('Bruce Springsteen', 'Born in the U.S.A', 1984, 22, 'Heartland rock'),
+('ABBA', 'Gold: Greatest Hits', 1992, 21.6, 'Pop,disco'),
+('Dire Straits', 'Brothers in Arms', 1985, 21.1, 'Roots rock, blue rock, soft rock'),
+('Santana', 'Supernatural', 1999, 20.8, 'Latin rock'),
+('Madonna', 'The immaculate Collection', 1990, 19.8, 'Pop, dance'),
+('Pink Floyd', 'The Wall', 1979, 18.9, 'Progressive rock'),
+('The Beatles', 'Sgt. Peppers Lonely Hearts Club Band', 1967, 18.3, 'Rock'),
+('Michale Jackson', 'Dangerous', 1991, 17.0, 'New jack swing, R&B and pop'),
+('The Beatles', 'Abbey Road', 1969, 16.9, 'Rock');
+
+
+
+
+
+
+
+
+# All albums in your table.
+SELECT record_name FROM albums;
+
+# All albums released before 1980
+SELECT record_name FROM albums WHERE release_date< 1980;
+
+# All albums by Michael Jackson
+SELECT record_name FROM albums WHERE artist = 'Michale Jackson';
+
---
+++ select_exercises.sql
@@ -0,0 +1,23 @@
+USE codeup_test_db;
+
+# The name of all albums by Pink Floyd.
+SELECT record_name FROM albums WHERE artist = 'Pink Floyd';
+
+# The year Sgt. Pepper's Lonely Hearts Club Band was released
+SELECT release_date FROM albums WHERE record_name = 'Sgt. Peppers Lonely Hearts Club Band';
+
+# The genre for Nevermind  is not on the top 2 tables
+SELECT genre FROM albums WHERE record_name = 'Nevermind';
+
+# Which albums were released in the 1990s
+SELECT record_name FROM albums WHERE release_date >= 1990;
+
+# Which albums had less than 20 million certified sales
+SELECT record_name FROM albums WHERE sales < 20;
+
+# All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+SELECT record_name FROM albums WHERE genre = 'rock';
+
+
+# As always, commit your changes after each step and push them out to GitHub
+
