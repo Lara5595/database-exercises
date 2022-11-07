23f1aceec736e775111a4a96971e71c1df956d0c
David Lara <david.a.lara05@gmail.com>
2022-11-07 10:50:08 -0600

    deleted and added back

--- basic_statements.sql
+++ basic_statements.sql
@@ -15,3 +15,29 @@ INSERT INTO albums (artist, record_name, release_date, sales, genre) VALUES ('Ba
 #  SELECT artist, record_name FROM albums WHERE sales < 1;  this gets the albums that have sales less than  million
 
 #  SELECT record_name, artist FROM albums WHERE sales BETWEEN 5 AND 50;
+
+
+# Using AS
+# SELECT record_name AS artist, artist FROM albums WHERE sales BETWEEN 5 AND 50;
+
+
+# Using UPDATE
+# This updated the name Tupac to 2Pac
+# UPDATE albums SET artist = '2Pac' WHERE artist = 'Tupac';
+
+# Another example is
+# UPDATE albums SET artist = 'Tupac Shakur' WHERE artist = '2Pac' AND sales < 1;
+
+
+#  This is how you modify columns
+# ALTER TABLE albums MODIFY COLUMN release_date DATE;
+
+# We change the name from artist to Artist
+# ALTER TABLE albums CHANGE COLUMN artist Artist VARCHAR(35);
+
+
+# DELETE
+# DELETE FROM albums WHERE id = 2;
+
+#  OR you can delete everything with
+#  TRUNCATE albums
