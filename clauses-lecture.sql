d20ba25113346a1b0fa4a5ee9c7e429e0f5725f5
David Lara <david.a.lara05@gmail.com>
2022-11-08 11:55:53 -0600

    finish assignment

--- clauses-lecture.sql
+++ clauses-lecture.sql
@@ -22,5 +22,15 @@ SELECT  emp_no, first_name, last_name FROM employees WHERE emp_no < 10026;
 SELECT emp_no, first_name, last_name FROM employees WHERE emp_no < 20000 AND last_name IN ('Herber', 'Baek') OR first_name = 'Shridhar';
 
 # You have to make sure you change a parenthesis if you are chaining if i didnt have it, it would return females also
-SELECT  first_name, last_name, gender FROM employees WHERE (last_name = 'Bolotov' OR last_name = 'Baek') AND gender = 'M';
+# We also you ORDERED BY
+SELECT  first_name, last_name, gender FROM employees WHERE (last_name = 'Bolotov' OR last_name = 'Baek') AND gender = 'M' ORDER BY last_name;
 
+
+# Using NOT BETWEEN
+SELECT * FROM salaries WHERE salary NOT BETWEEN 50000 AND 100000;
+
+# Using NOT LIKE
+SELECT first_name, birth_date, hire_date FROM  employees WHERE birth_date NOT LIKE '195%' AND  hire_date NOT LIKE '199%';
+
+
+SELECT * FROM employees WHERE birth_date LIKE '1954-01%' ORDER BY birth_date;
